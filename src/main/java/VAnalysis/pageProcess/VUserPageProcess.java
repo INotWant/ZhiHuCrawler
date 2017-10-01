6c81cc8abc2c70b238b5939e738acfd4f444143f
jie8888 <kiss.xiaojie15@gmail.com>
2017-10-01 08:56:17 +0800

    爬取大V资料（end）

--- src/main/java/VAnalysis/pageProcess/VUserPageProcess.java
+++ src/main/java/VAnalysis/pageProcess/VUserPageProcess.java
@@ -20,6 +20,7 @@ import java.util.List;
  */
 public class VUserPageProcess implements PageProcessor {
 
+    private int offset = 0;
     private VUser vUser = new VUser();
     private Site site = Site
             .me()
@@ -45,33 +46,22 @@ public class VUserPageProcess implements PageProcessor {
                 page.addTargetRequest("https://www.zhihu.com/question/" + questionList.get(i) + "/answer/" + answerList.get(i));
             }
         } else if (url.matches("^(https://www.zhihu.com/people/).*")) {
-            List<String> urlList = page.getHtml().links().regex("https://www.zhihu.com/question/[\\d+]+/answer/[\\d+]+").all();
-            page.addTargetRequests(urlList);
-            String jsonUrl = "https://www.zhihu.com/api/v4/members/" + url.substring(url.indexOf("people/") + 7, url.indexOf("/answers")) + "/answers?include=data%5B*%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Cmark_infos%2Ccreated_time%2Cupdated_time%2Creview_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cvoting%2Cis_author%2Cis_thanked%2Cis_nothelp%2Cupvoted_followees%3Bdata%5B*%5D.author.badge%5B%3F(type%3Dbest_answerer)%5D.topics&offset=5&limit=15&sort_by=created&page=1";
-            page.addTargetRequest(jsonUrl);
-            if (url.charAt(url.length() - 2) == '=' && url.charAt(url.length() - 1) == '1') {
-                List<String> pageList = page.getHtml().xpath("//button[@class=\"Button PaginationButton Button--plain\"]/text()").all();
-                String urlPrefix = url.substring(0, url.lastIndexOf('=') + 1);
-                for (int i = 2; i < Integer.parseInt(pageList.get(pageList.size() - 1)); i++) {
-                    page.addTargetRequest(urlPrefix + i);
-                    page.addTargetRequest(jsonUrl.substring(0, jsonUrl.lastIndexOf("=") + 1) + i);
-                }
-                String userName = page.getHtml().xpath("//span[@class=\"ProfileHeader-name\"]/text()").get();
-                vUser.setUserName(userName);
-                Query query = session.createQuery("from VUser where userName = ?");
-                query.setParameter(0, userName);
-                List list = query.list();
-                if (list == null || list.size() == 0) {
-                    try {
-                        session.save(vUser);
-                        transaction.commit();
-                    } catch (Exception e) {
-                        if (transaction != null)
-                            transaction.rollback();
-                    }
-                } else
-                    vUser = (VUser) list.get(0);
+            int answerTotalNum = Integer.parseInt(page.getHtml().xpath("//span[@class=\"Tabs-meta\"]/text()").get());
+            for (int i = 0; i <= answerTotalNum / 20; i++) {
+                String jsonUrl = "https://www.zhihu.com/api/v4/members/" + url.substring(url.indexOf("people/") + 7, url.indexOf("/answers")) + "/answers?include=data%5B*%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Cmark_infos%2Ccreated_time%2Cupdated_time%2Creview_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cvoting%2Cis_author%2Cis_thanked%2Cis_nothelp%2Cupvoted_followees%3Bdata%5B*%5D.author.badge%5B%3F(type%3Dbest_answerer)%5D.topics&offset="
+                        + offset + "&limit=20&sort_by=created&page=1";
+                offset += 20;
+                page.addTargetRequest(jsonUrl);
             }
+            String userName = page.getHtml().xpath("//span[@class=\"ProfileHeader-name\"]/text()").get();
+            vUser.setUserName(userName);
+            Query query = session.createQuery("from VUser where userName = ?");
+            query.setParameter(0, userName);
+            List list = query.list();
+            if (list == null || list.size() == 0) {
+                saveObj(session, transaction, vUser);
+            } else
+                vUser = (VUser) list.get(0);
         } else {
             String timeStr = page.getHtml().xpath("//div[@class=\"ContentItem-time\"]/a/span/@data-tooltip").get();
             if (timeStr == null)
@@ -84,19 +74,23 @@ public class VUserPageProcess implements PageProcessor {
             answer.setFollower(Integer.valueOf(followerStr));
             String question = page.getHtml().xpath("//h1[@class=\"QuestionHeader-title\"]/text()").get();
             answer.setQuestion(question);
-            try {
-                session.save(answer);
-                transaction.commit();
-            } catch (Exception e) {
-                if (transaction != null)
-                    transaction.rollback();
-            }
+            saveObj(session, transaction, answer);
         }
     }
 
     @Override
     public Site getSite() {
         return site;
+    }
+
+    private <T> void saveObj(Session session, Transaction transaction, T obj) {
+        try {
+            session.save(obj);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null)
+                transaction.rollback();
+        }
     }
 
     public static void main(String[] args) throws InterruptedException {
